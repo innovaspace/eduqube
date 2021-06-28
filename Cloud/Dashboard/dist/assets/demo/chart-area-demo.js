fc6f9381b90d9fe4369f63db67d08b373eee3374
Luca Uriarte <42620882+lucauriarte@users.noreply.github.com>
2021-06-27 23:51:32 -0300

    Add files via upload

---
+++ Cloud/Dashboard/dist/assets/demo/chart-area-demo.js
@@ -0,0 +1,58 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var dt = new Date();
+  document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+var ctx = document.getElementById("myAreaChart");
+var datos = 33333;
+var horas = "00:11:11";
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [dt, "00:00:00", "00:00:00", horas, "00:00:00" , "00:00:00", "00:00:00"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [10000, 30162, 26263, datos, 18287, 28682, 31274,],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 40000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
